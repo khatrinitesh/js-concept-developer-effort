14e57b4f61e7d0cc0a8afcbf0e608e02b7594529
niteshkhatri <nitesh.khatri88@gmail.com>
2023-01-12 22:21:26 +0530

    added features

--- js-concept-developer.js
+++ js-concept-developer.js
@@ -1,3 +1,17 @@
+// 1 ternary operator
+// 2 destructing
+// 3 spread opeartor
+// 4 array methods
+// 5 arrow functions
+// 6 promises
+// 7 fetch api
+// 9 async / await
+// 10 es6 modules and import/export
+// 11 hositing
+// 12 closures
+// 13 scope
+
+//***  START - January 12th, Thursday ***//
 const fullname = 'nitesh khatri'
 console.log(fullname)
 
@@ -12,3 +26,37 @@ const resultfriends = friends.sort(function(a,b){
     return -1
 })
 console.log(resultfriends);
+
+// the ternary operator
+const isEnable = false 
+console.log(isEnable ? 'nitesh' : 'sameet')
+
+// destructing 
+const objects = ['table','iphone','apple'];
+const [table,...rest] = objects 
+// const furniture = objects[0]
+// const mobile = objects[1]
+// const fruit = objects[2]
+console.log(table);
+console.log(...rest);
+
+// the fetch api
+// const fetchdata = fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(response => response.json())
+// .then((data) => setState(data));
+// console.log(fetchdata)
+
+// async / await
+async function  asyncFunction(){
+    let promise = new Promise(resolve => {
+        resolve();
+    });
+    let response = await promise;
+    return console.log(response);
+};
+
+var a = 5;
+console.log(5);
+
+
+//***  END - January 12th, Thursday ***//
